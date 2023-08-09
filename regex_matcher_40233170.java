f54dd480e3dd709975299011af9048c9d11e0e49
Aryan <aryan.saxena094@gmail.com>
2023-08-09 19:03:39 -0400

    commit

--- regex_matcher_40233170.java
+++ regex_matcher_40233170.java
@@ -4,9 +4,7 @@
 // Imports
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Collections;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
+import java.util.PriorityQueue;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -35,61 +33,44 @@ class regex_matcher_40233170 {
     }
 
     public static String LCS3(String A, String B, String C) {
-        // Getting lengths of all three for the constructing of the matrices
-        int m = A.length();
-        int n = B.length();
-        int o = C.length();
+        String lcsOfFirstTwo = LCS(A, B);
+        return LCS(lcsOfFirstTwo, C);
+    }
 
-        // Getting the characters in lower case to perform the comparison case
-        // insensitive
-        char[] aChars = A.toLowerCase().toCharArray();
-        char[] bChars = B.toLowerCase().toCharArray();
-        char[] cChars = C.toLowerCase().toCharArray();
-
-        // Initializing a matrix for DP
-        int[][][] L = new int[m + 1][n + 1][o + 1];
-
-        // Filling values in the DP (Going from top left to bottom right)
-        for (int i = 0; i <= m; i++) {
-            for (int j = 0; j <= n; j++) {
-                for (int k = 0; k <= o; k++) {
-                    if (i == 0 || j == 0 || k == 0) {
-                        // For 0th column and 0th row
-                        L[i][j][k] = 0;
-                    } else if (aChars[i - 1] == bChars[j - 1] && aChars[i - 1] == cChars[k - 1]) {
-                        // If characters match for all three strings
-                        L[i][j][k] = L[i - 1][j - 1][k - 1] + 1;
-                    } else {
-                        // Getting the last longest LCS among strings (If current doesn't match)
-                        L[i][j][k] = Math.max(Math.max(L[i - 1][j][k], L[i][j - 1][k]), L[i][j][k - 1]);
+    public static boolean isRegexMatch(String inputString, String regexPattern) {
+        int m = inputString.length();
+        int n = regexPattern.length();
+    
+        // dp[i][j] will be true if the first i characters in inputString match the first j characters in regexPattern
+        boolean[][] dp = new boolean[m + 1][n + 1];
+    
+        // An empty pattern matches an empty string
+        dp[0][0] = true;
+    
+        // Handle patterns like a* or a*b* or a*b*c*
+        for (int j = 1; j <= n; j++) {
+            if (regexPattern.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+    
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (regexPattern.charAt(j - 1) == '.' || regexPattern.charAt(j - 1) == inputString.charAt(i - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (regexPattern.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i][j - 2]; // Consider the pattern without the preceding character and '*'
+                    if (regexPattern.charAt(j - 2) == '.' || regexPattern.charAt(j - 2) == inputString.charAt(i - 1)) {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j];
                     }
+                } else {
+                    dp[i][j] = false;
                 }
             }
         }
-
-        int i = m, j = n, k = o;
-        StringBuilder lcs = new StringBuilder();
-
-        // Reconstructing
-        while (i > 0 && j > 0 && k > 0) {
-            if (aChars[i - 1] == bChars[j - 1] && aChars[i - 1] == cChars[k - 1]) {
-                // If it matches the add to the string
-                lcs.append(A.charAt(i - 1)); // Use original string to preserve case
-                i--;
-                j--;
-                k--;
-            } else if (L[i - 1][j][k] >= L[i][j - 1][k] && L[i - 1][j][k] >= L[i][j][k - 1]) {
-                i--;
-            } else if (L[i][j - 1][k] >= L[i - 1][j][k] && L[i][j - 1][k] >= L[i][j][k - 1]) {
-                j--;
-            } else {
-                k--;
-            }
-        }
-        // Reverse the string to get lcs because characters are appended
-        return lcs.reverse().toString();
+        return dp[m][n];
     }
-
+    
     public static String LCS(String A, String B) {
         // Getting lengths in order to construct the matrices
         int m = A.length();
@@ -142,60 +123,54 @@ class regex_matcher_40233170 {
         long startTime = System.currentTimeMillis();
 
         // Reading data from the input file using the function input
-        List<String> inputFile = input("input.txt");
+        List<String> data = input("input.txt");
+
+        System.out.println(data);
 
         // Logic
         // Taking the size of the dictionary i.e n
-        int n = Integer.parseInt(inputFile.get(0));
-        System.out.println(inputFile);
+        int n = Integer.parseInt(data.get(0));
 
-        // Making a pattern of the last word (i.e the Pattern)
-        Pattern pattern = Pattern.compile(inputFile.get(n + 1), Pattern.CASE_INSENSITIVE);
+        PriorityQueue<String> sortedStrings = new PriorityQueue<>(String::compareToIgnoreCase);
+        sortedStrings.addAll(data.subList(1, data.size() - 1));
+        
+        System.out.println(sortedStrings);
 
-        // Initializing a matcher
-        Matcher matcher;
+        // Getting the pattern
+        String regexString = data.get(n+1);
 
         // Making an ArrayList to store the matching words
-        List<String> matchingWords = new ArrayList<>();
+        data = new ArrayList<>();
 
         // Filtering the matching words from the arraylist
-        for (int i = 1; i <= n; i++) {
-            matcher = pattern.matcher(inputFile.get(i));
-            if (matcher.matches()) {
-                matchingWords.add(inputFile.get(i));
+        while(!sortedStrings.isEmpty() && data.size() < 3){
+            String current = sortedStrings.poll();
+            if (isRegexMatch(current, regexString)) {
+                data.add(current);
             }
         }
-
-        // Case-insensitively soting the matching words
-        Collections.sort(matchingWords, String.CASE_INSENSITIVE_ORDER);
-        System.out.println(matchingWords);
-
-        // Initializing output String
-        String output;
+        
+        System.out.println(data);
 
         // Switch cases to deal with the number of matching words we get
-        switch (matchingWords.size()) {
+        switch (data.size()) {
             // If 3 matching words then LCS of those three
             case 3:
-                output = LCS3(matchingWords.get(0), matchingWords.get(1), matchingWords.get(2));
+                writeOutput(LCS3(data.get(0), data.get(1), data.get(2)));
                 break;
             // If 2 matching words then normal LCS of those two
             case 2:
-                output = LCS(matchingWords.get(0), matchingWords.get(1));
+                writeOutput(LCS(data.get(0), data.get(1)));
                 break;
             // Else output the only matching string straight out
             case 1:
-                output = matchingWords.get(0);
+                writeOutput(data.get(0));
                 break;
             // If there are no matching words with the last string then there would be no
             // output
             default:
-                output = "";
+                writeOutput("");
         }
-
-        System.out.println(output);
-        // Writing the output in the output file using the output string
-        writeOutput(output);
 
         // Logging time for debugging the running time
         long endTime = System.currentTimeMillis();
